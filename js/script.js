a3eaafe80e7c4867dce2facd8eb22b107d4b29a9
YogaMobisvit <YogaMobisvit@gmail.com>
2020-08-06 19:42:43 +0200

    add timer

--- js/script.js
+++ js/script.js
@@ -37,4 +37,53 @@ window.addEventListener('DOMContentLoaded', function () {
          }
       }
    });
+
+   //! timer
+
+   // let deadline = '2020-08-07';
+   const deadline = new Date(Date.parse(new Date()) + 20 * 60 * 60 * 1000);
+
+
+
+
+
+   function getTimeRemaining(endtime) {
+      let t = Date.parse(endtime) - Date.parse(new Date()),
+         seconds = Math.floor((t / 1000) % 60),
+         minutes = Math.floor((t / 1000 / 60) % 60),
+         hours = Math.floor((t / (1000 * 60 * 60)));
+
+      return {
+         'total': t,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds
+      };
+
+   }
+
+   function setClock(id, endtime) {
+      let timer = document.getElementById(id),
+         hoursSpan = timer.querySelector('.hours'),
+         minutesSpan = timer.querySelector('.minutes'),
+         secondsSpan = timer.querySelector('.seconds'),
+         timeInterval = setInterval(updateClock, 1000);
+
+
+
+      function updateClock() {
+         let t = getTimeRemaining(endtime);
+         hoursSpan.textContent = ('0' + t.hours).slice(-2);
+         minutesSpan.textContent = ('0' + t.minutes).slice(-2);
+         secondsSpan.textContent = ('0' + t.seconds).slice(-2);
+
+         if (t.total <= 0) {
+            clearInterval(timeInterval);
+         }
+
+      }
+   }
+   setClock('timer', deadline);
+   // setClock('timer', deadline);
+
 });
